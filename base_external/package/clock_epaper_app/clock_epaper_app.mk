d2113ccc2e52d21bb1d998993acc85d8f22ea85d
meshal <meshal.alhazmi@gmail.com>
2026-01-28 08:15:59 -0600

    point to github repo and enable build with debug

--- base_external/package/clock_epaper_app/clock_epaper_app.mk
+++ base_external/package/clock_epaper_app/clock_epaper_app.mk
@@ -4,13 +4,16 @@
 #
 ################################################################################
 
-CLOCK_EPAPER_APP_VERSION = 1.0
-CLOCK_EPAPER_APP_SITE = /home/meshal/Desktop/courses/ECEA_5305/final_project/final_app
-CLOCK_EPAPER_APP_SITE_METHOD = local
+CLOCK_EPAPER_APP_VERSION = 49f997ab3d80a9f172d34d037cb51b2bc97f7b27
+CLOCK_EPAPER_APP_SITE = https://github.com/meshalalhazmi/ecea5307-final-e-paper
+CLOCK_EPAPER_APP_SITE_METHOD = git
 CLOCK_EPAPER_APP_DEPENDENCIES = libgpiod
 
 define CLOCK_EPAPER_APP_BUILD_CMDS
-	$(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D)
+	$(MAKE) $(TARGET_CONFIGURE_OPTS) \
+		CPPFLAGS="$(TARGET_CPPFLAGS) -DDEBUG -DRPI -DUSE_DEV_LIB -Iinclude -Ithird_party/waveshare_e_paper/RaspberryPi_JetsonNano/c/lib/Config -Ithird_party/waveshare_e_paper/RaspberryPi_JetsonNano/c/lib/e-Paper -Ithird_party/waveshare_e_paper/RaspberryPi_JetsonNano/c/lib/GUI -Ithird_party/waveshare_e_paper/RaspberryPi_JetsonNano/c/lib/Fonts" \
+		LDFLAGS="$(TARGET_LDFLAGS) -lgpiod" \
+		-C $(@D)
 endef
 
 define CLOCK_EPAPER_APP_INSTALL_TARGET_CMDS
